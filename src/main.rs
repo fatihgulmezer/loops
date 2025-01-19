86dd3dc483846b4c298b7b18c594a25bf5ae1074
fatihgulmezer <fatihgulmezer@gmail.com>
2025-01-19 16:11:46 +0300

    Added features

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
    
     for number in (1..4).rev() {
-        println!("the value is: {number}");
+        println!("the value is^+++++: {number}");
+        println!("the value is ^^^^: {number}");
     }
 }
